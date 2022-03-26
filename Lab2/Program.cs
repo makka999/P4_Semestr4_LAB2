b09c759bd4524beb15d10e9c697c112b81ad98c0
makka999 <55660660+makka999@users.noreply.github.com>
2022-03-26 20:28:26 +0100

    wykonanie zadania

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,4 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;" +
+    "Initial Catalog=Northwind;" +
+    "Integrated Security=True;" +
+    "Connect Timeout=30;" +
+    "Encrypt=False;" +
+    "TrustServerCertificate=False;" +
+    "ApplicationIntent=ReadWrite;" +
+    "MultiSubnetFailover=False";
+
+var conn = new SqlConnection(connectionString);
+conn.Open();
+
+var getQueryString = "SELECT * FROM [dbo].[Region]";
+var setQueryString = "INSERT INTO [dbo].[Region] ([RegionID],[RegionDescription]) VALUES (6,'slask')"; 
+var getQueryStringCommand = new SqlCommand(getQueryString, conn);
+var setQueryStringCommand = new SqlCommand(setQueryString, conn);
+
+var add = setQueryStringCommand.ExecuteNonQuery();
+
+var reader = getQueryStringCommand.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine(reader.GetInt32(0) + " " + reader["RegionDescription"]);
+    //Console.WriteLine($"{reader.GetInt32(0)} { reader["RegionDescription"]}");
+}
+
+conn.Close();
