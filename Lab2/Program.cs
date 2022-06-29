c5e5ec866aa81fb33b4d10a16100c0b592534263
makka999 <55660660+makka999@users.noreply.github.com>
2022-06-29 04:10:47 +0200

    Dodany parametr

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,22 +1,26 @@
-﻿using System.Data.SqlClient;
-var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;" +
-    "Initial Catalog=Northwind;" +
-    "Integrated Security=True;" +
-    "Connect Timeout=30;" +
-    "Encrypt=False;" +
-    "TrustServerCertificate=False;" +
-    "ApplicationIntent=ReadWrite;" +
-    "MultiSubnetFailover=False";
+﻿using System.Data;
+using System.Data.SqlClient;
+var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; 
 
 var conn = new SqlConnection(connectionString);
 conn.Open();
 
+var parametrID = new SqlParameter("@ID", SqlDbType.Int);
+//parametrID.Direction = ParameterDirection.Input;
+parametrID.Value = 13;
+
+var parametrName = new SqlParameter("@Name", SqlDbType.NChar);
+parametrName.Value = "mazowieckie";
+
 var getQueryString = "SELECT * FROM [dbo].[Region]";
-var setQueryString = "INSERT INTO [dbo].[Region] ([RegionID],[RegionDescription]) VALUES (6,'slask')"; 
+var setQueryString = "INSERT INTO [dbo].[Region] ([RegionID],[RegionDescription]) VALUES (@ID,@Name)"; 
 var getQueryStringCommand = new SqlCommand(getQueryString, conn);
 var setQueryStringCommand = new SqlCommand(setQueryString, conn);
+setQueryStringCommand.Parameters.Add(parametrID);
+setQueryStringCommand.Parameters.Add(parametrName);
 
-var add = setQueryStringCommand.ExecuteNonQuery();
+
+setQueryStringCommand.ExecuteNonQuery();
 
 var reader = getQueryStringCommand.ExecuteReader();
 
